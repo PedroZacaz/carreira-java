276a44573c112cdb8f4d357a5da98f6d6aa81830
Pedro Zacarias <pedroproli@hotmail.com>
2026-03-15 14:30:01 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun Maverick");
+
+        //      TIPOS PRIMITIVOS
+        // camelCase ↓
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean inclidoNoPlano = true;
+        double notaFilme = 8.1;
+
+
+    }
+}
+
---
+++ src/Operacoes.java
@@ -0,0 +1,9 @@
+public class Operacoes {
+    public static void main(String[] args) {
+        System.out.println("Isso é uma Soma de 10 + 5");
+        System.out.println(10 + 5);
+
+        System.out.println("Isso é uma Subtração de 10 - 5");
+        System.out.println(10 - 5);
+    }
+}
